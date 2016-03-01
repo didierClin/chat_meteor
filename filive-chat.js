863de04a51e886a1e4a4a40505d41d00fa0e3c50
didierClin <didier.c62@numericable.fr>
2016-03-01 18:42:51 +0100

    enter message in textarea

--- filive-chat.js
+++ filive-chat.js
@@ -1,19 +1,32 @@
+Messages = new Mongo.Collection('messages');
+
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('counter', 0);
+  // Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  Template.messages.helpers({
+    messages: function () {
+      // return Messages.find({message: 'Hello World2!'});
+      return Messages.find({});
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.messages.events({
+    'keypress textarea': function(e, instance) {
+      if (e.keyCode == 13) { // enter key pressed
+        var value = instance.find('textarea').value;
+        instance.find('textarea').value = '';
+
+        Messages.insert({
+          message: value,
+          timestamp: new Date(),
+          // user: Meteor.user_id()
+        });
+      }
     }
-  });
+
+  })
+
 }
 
 if (Meteor.isServer) {
